41a0c1dbb6d91ac3f63b15075ad32df870aad678
ipancaldo <ignacio_pancaldo@live.com>
2022-11-13 20:12:07 -0300

    Added Products view and its sidebar

--- RedAndWhite.Users.UI/wwwroot/js/site.js
+++ RedAndWhite.Users.UI/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+});
+
+const navToggle = document.querySelector('.nav-toggle');
+
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
