e533a5ccc174533c7e39a7ecdd4d2bb7fffda93d
ipancaldo <ignacio_pancaldo@live.com>
2022-12-07 20:51:33 -0300

    Cleaning css styles and re organizing some html.

--- RedAndWhite.Users.UI/src/app/app-routing.module.ts
+++ RedAndWhite.Users.UI/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductsComponent } from './views/products/products.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  // { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'productos', component: ProductsComponent },
   // { path: 'producto', component: ProductoComponent },
   // { path: 'producto/:id', component: ProductoComponent },
--- RedAndWhite.Users.UI/src/app/app.module.ts
+++ RedAndWhite.Users.UI/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { ProductsComponent } from './views/products/products.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent, ProductsComponent],
+  declarations: [AppComponent, ProductsComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
