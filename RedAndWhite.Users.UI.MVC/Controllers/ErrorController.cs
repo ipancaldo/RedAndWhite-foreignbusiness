f2a3f418467b0e4aab1ef38e1ba735031b6f24c7
Ignacio Pancaldo <ignacio_pancaldo@live.com>
2024-01-09 19:28:24 +0100

    Added error page.

---
+++ RedAndWhite.Users.UI.MVC/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class ErrorController : Controller
+{
+    // GET: Error
+    public ActionResult Index(Exception ex)
+    {
+        // Log the exception or perform any other error handling logic here
+
+        // Pass the exception details to the view
+        ViewBag.Exception = ex;
+
+        return View();
+    }
+}
