9a28ccb4b5b361b9cb85835b2071a9893382693f
Ignacio Pancaldo <ignacio_pancaldo@live.com>
2024-02-09 21:24:39 +0100

    Added Test project using xUnit

---
+++ RedAndWhite.Test/BaseTest.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using RedAndWhite.Data;
+using RedAndWhite.Infrastructure.Mapping;
+using RedAndWhite.Repository.Brands;
+using RedAndWhite.Repository.Categories;
+using RedAndWhite.Repository.Informations;
+using RedAndWhite.Repository.Products;
+using RedAndWhite.Service.Brands;
+using RedAndWhite.Service.Categories;
+using RedAndWhite.Service.Common;
+using RedAndWhite.Service.Informations;
+using RedAndWhite.Service.Products;
+using System.Reflection;
+
+namespace RedAndWhite.Test
+{
+    public class BaseTest : IClassFixture<ServiceProviderFixture>
+    {
+        protected readonly IServiceProvider _serviceProvider;
+
+        public BaseTest(ServiceProviderFixture serviceProviderFixture)
+        {
+            _serviceProvider = serviceProviderFixture.ServiceProvider;
+        }
+    }
+
+    public class ServiceProviderFixture : IDisposable
+    {
+        public ServiceProvider ServiceProvider { get; }
+
+        public ServiceProviderFixture()
+        {
+            ServiceProvider = BootIoC();
+        }
+
+        public void Dispose()
+        {
+            // Dispose of any resources if needed
+        }
+
+        private static ServiceProvider BootIoC()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+            IMapper mapper = mapperConfig.CreateMapper();
+            serviceCollection.AddSingleton(mapper);
+
+            serviceCollection.AddData("data source=(local); initial catalog=RedAndWhite;Integrated Security=True");
+            serviceCollection.AddScoped<IProductRepository, ProductRepository>();
+            serviceCollection.AddScoped<IInformationService, InformationService>();
+            serviceCollection.AddScoped<IProductService, ProductService>();
+            serviceCollection.AddScoped<IResultVerifierService, ResultVerifierService>();
+            RegisterAsImplementedInterfaces<CategoryService>(serviceCollection, ServiceLifetime.Scoped);
+            RegisterAsImplementedInterfaces<BrandService>(serviceCollection, ServiceLifetime.Scoped);
+
+            serviceCollection.AddScoped<IProductRepository, ProductRepository>();
+            serviceCollection.AddScoped<IBrandRepository, BrandRepository>();
+            serviceCollection.AddScoped<ICategoryRepository, CategoryRepository>();
+            serviceCollection.AddScoped<IInformationRepository, InformationRepository>();
+
+            return serviceCollection.BuildServiceProvider();
+        }
+
+        private static void RegisterAsImplementedInterfaces<TService>(IServiceCollection services, ServiceLifetime lifetime)
+        {
+            var interfaces = typeof(TService).GetTypeInfo().ImplementedInterfaces
+                                .Where(i => i != typeof(IDisposable) && i.IsPublic);
+
+            foreach (var interfaceType in interfaces)
+            {
+                services.Add(new ServiceDescriptor(interfaceType, typeof(TService), lifetime));
+            }
+        }
+    }
+}
---
+++ RedAndWhite.Test/Products/CreateProductTest.cs
@@ -0,0 +1,44 @@
+﻿using RedAndWhite.Model.Products;
+using RedAndWhite.Service.Products;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RedAndWhite.Test
+{
+    public class CreateProductTest : BaseTest
+    {
+        private readonly IProductService _productService;
+
+        public CreateProductTest(ServiceProviderFixture serviceProviderFixture) : base(serviceProviderFixture)
+        {
+            _productService = _serviceProvider.GetRequiredService<IProductService>();
+        }
+
+        private const string _productName = "Test product name";
+        private const string _productDescription = "Test product description";
+
+        [Fact]
+        public async Task Should_Create_New_Product()
+        {
+            //Arrange
+            NewProductModel newProductModel = new NewProductModel()
+            {
+                Name = _productName,
+                Description = _productDescription
+            };
+
+            //Act
+            await _productService.Create(newProductModel);
+
+            //Assert
+            var productFound = _productService.GetAllProducts().FirstOrDefault(p => p.Name.Equals(_productName));
+            Assert.NotNull(productFound);
+            Assert.Equal(_productName, productFound.Name);
+            Assert.Equal(_productDescription, productFound.Description);
+
+            //Clean up
+            await _productService.Delete(productFound.Id);
+            productFound = _productService.GetAllProducts().FirstOrDefault(p => p.Name.Equals(_productName));
+            Assert.Null(productFound);
+        }
+    }
+}
